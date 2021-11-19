5fba64cc265e68fc33d888a4a58efaa5d583f52b
MaryMaina98 <marieshix98@gmail.com>
2021-11-19 11:22:10 +0300

    modal pop up created

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,70 +1,78 @@
-import React,{useState,useContext} from 'react'
-import styled from 'styled-components'
-import CartItem from '../components/CartItem';
-import { Context } from '../Context';
+import React, { useState, useContext } from "react";
+import styled from "styled-components";
+import CartItem from "../components/CartItem";
+import { Context } from "../Context";
+import Modal from "../components/Modal";
 
 const Cart = () => {
-    const {cartItems, emptyCart} = useContext(Context)
-    const [order, setOrder] = useState(false)
+  const { cartItems, emptyCart } = useContext(Context);
+  const [order, setOrder] = useState(false);
+  const [modal, setModal] = useState(false);
 
-    const totalCost = (cartItems.length * 499).toLocaleString("en-US", {style: "currency", currency: "USD"})
-    console.log(cartItems)
+  const totalCost = (cartItems.length * 499).toLocaleString("en-US", {
+    style: "currency",
+    currency: "USD",
+  });
+  console.log(cartItems);
+  const handleModal = () => {
+    setModal(true);
+  };
 
-   const checkOut= () =>{
-    setOrder(true) 
-    setTimeout(()=>{setOrder(false)
-     emptyCart()},5000)
-   }
-   const handleOrder = () =>{
-       if(totalCost === "$0.00"){
-           return false
-       }else{
-           return checkOut()
-       }
-       
-       
-      
-      
-       
-   }
-    return (
-        <CartWrapper>
-            <h1> Checkout</h1> 
-            <div>
-               {cartItems.map(({author, id, download_url},index)=>
-               <CartItem key={index} id={id} author={author} url={download_url} />
-               )}
-            </div>
-        
-            <p>Total: {totalCost}</p>
-            <PlaceOrder>
-                <button onClick={handleOrder}>{order ? "Ordering...": "Place order" }</button>
-            </PlaceOrder>
-        </CartWrapper>
-    )
-}
+  const checkOut = () => {
+    setOrder(true);
+    setTimeout(() => {
+      setOrder(false);
+      emptyCart();
+    }, 3000);
+  };
+  const handleOrder = () => {
+    if (totalCost === "$0.00") {
+      return handleModal();
+    } else {
+      return checkOut();
+    }
+  };
+  return (
+    <CartWrapper>
+      <h1> Checkout</h1>
+      <div>
+        {cartItems.map(({ author, id, download_url }, index) => (
+          <CartItem key={index} id={id} author={author} url={download_url} />
+        ))}
+      </div>
 
-export default Cart
+      <p>Total: {totalCost}</p>
+      <PlaceOrder>
+        <button onClick={handleOrder}>
+          {order ? "Ordering..." : "Place order"}
+        </button>
+      </PlaceOrder>
+      {modal && <Modal />}
+    </CartWrapper>
+  );
+};
+
+export default Cart;
 
 const CartWrapper = styled.div`
-padding: 20px 100px;
-width: 80%;
-margin: 10px auto;
-box-shadow: 0 0 3px #777;
-@media(max-width: 768px){
+  padding: 20px 100px;
+  width: 80%;
+  margin: 10px auto;
+  box-shadow: 0 0 3px #777;
+  @media (max-width: 768px) {
     width: 100%;
-}
-h1{
+  }
+  h1 {
     text-align: center;
     font-weight: bold;
-}
-p{
+  }
+  p {
     text-align: right;
-}
-div{
+  }
+  div {
     text-align: center;
-}
-`
+  }
+`;
 const PlaceOrder = styled.div`
 button{
     padding: 20px 30px;
@@ -76,4 +84,4 @@ button{
     outline: none;
     border-radius: 10px;
 }
-`
+`;
